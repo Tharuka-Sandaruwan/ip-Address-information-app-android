7c1862532cce5c700da7194c458499aa6e3c4146
Tharuka Sandaruwan <ts2000614@gmail.com>
2023-01-10 12:16:57 +0530

    imported neeeded packages

--- app/src/main/java/com/example/ipaddressfinder/MainActivity.java
+++ app/src/main/java/com/example/ipaddressfinder/MainActivity.java
@@ -4,6 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+import androidx.appcompat.app.AlertDialog;
+
+
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.format.Formatter;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
